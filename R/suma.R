77d22ce3a6540794f1dd5fa1789c1071d1dc60c0
MatiFasolato <fasolatomatias@gmail.com>
2024-10-14 18:52:34 -0300

    Agregamos función suma() y la respectiva documentación

--- R/suma.R
+++ R/suma.R
@@ -1,3 +1,20 @@
+#' Suma de dos numeros positivos
+#'
+#' La función `suma()` dos valores numéricos positivos y calcula la suma entre ambos números.
+#'
+#' Estos son detalles extras de la función.
+#'
+#' @param x un vector numérico positivo
+#' @param y un vector numérico positivo
+#'
+#' @return
+#' Un vector numérico con la suma de `x` e `y`.
+#'
+#' @examples
+#' suma(10,10)
+#' suma(5,14)
+#'
+#' @export
 suma <- function(x, y) {
   if (!is.numeric(x) | !is.numeric(y)) {
     return("ERROR!, deben ser numéricos")

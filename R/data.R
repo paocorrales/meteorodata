541dfa64c187cf9932634e96160d2cc2c5121e66
MatiFasolato <fasolatomatias@gmail.com>
2024-10-15 20:56:42 -0300

    Actualizamos la documentación de los datos en el archivo data.R

--- R/data.R
+++ R/data.R
@@ -10,3 +10,16 @@
 #'
 #' @source Fuente: generación propia con funciones de R.
 "datos"
+
+
+#' Segundo conjunto de datos
+#'
+#' Los datos contienen información de las distintas estaciones meteorológicas.
+#'
+#' @format Los datos son obtenidos de un repositorio de GitHub.
+#' \describe{
+#' \item{NH0098}{muestra de datos de la estación "NH0098"}
+#' }
+#'
+#' @source Fuente: repositorio de GitHub.
+"datos_estación"

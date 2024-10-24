05761a006cc10857961204d12550296309cc0900
bautitorre12 <bautistatorregiani@gmail.com>
2024-10-24 19:57:46 -0300

    Agrego Función C_to_F

---
+++ R/C_to_F.R
@@ -0,0 +1,29 @@
+#' Conversión de la temperatura en Centigrados a Fahrenheit
+#'
+#' La función `C_to_F()` toma un valor numérico de la temperatura en Centigrados y calcula la equivalencia en grados fahrenheit.
+#'
+#' Estos son detalles extras de la función.
+#'
+#' @param temp_fahrenheit un vector numérico de la temperatura en Centigrados
+#'
+#' @return
+#' Un vector numérico de la temperatura en grados fahrenheit.
+#'
+#' @examples
+#' C_to_F(23)
+#'
+#' @export
+#'
+C_to_F <- function(temp_centigrado) {
+
+  if (!is.numeric(temp_centigrado)) {
+
+    cli::cli_abort(c(
+      "El argumento temp_centigrado debe ser numérico." ,
+      "i" = "La variable ingresada es un {class(temp_centigrado)[1]}"
+    ))
+  }
+  (temp_centigrado * 9/5) + 32
+}
+
+

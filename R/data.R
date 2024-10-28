566ba83ad6df5265f88b828d39a2dd5694dbe27f
MatiFasolato <fasolatomatias@gmail.com>
2024-10-28 11:29:10 -0300

    Modificamos la documentación de los dataset disponibles en meteorodata

--- R/data.R
+++ R/data.R
@@ -12,18 +12,272 @@
 "datos"
 
 
-#' Datos de estaciones meteorológicas
+#' Datos de la estación meteorológica NH0098
 #'
-#' Este conjunto de datos incluye muestras de diferentes estaciones meteorológicas, obtenidas de un repositorio en GitHub.
+#' Este conjunto de datos incluye mediciones de la estación meteorológica "NH0098".
 #'
-#' @format Un dataframe con las siguientes estaciones y columnas:
+#' @format Un dataframe con las siguientes columnas:
 #' \describe{
-#'   \item{NH0098}{Datos de la estación meteorológica "NH0098".}
-#'   \item{NH0437}{Datos de la estación meteorológica "NH0437".}
-#'   \item{NH0046}{Datos de la estación meteorológica "NH0046".}
-#'   \item{NH0910}{Datos de la estación meteorológica "NH0910".}
-#'   \item{NH0472}{Datos de la estación meteorológica "NH0472".}
-#'   \item{NH_unidos}{Datos combinados de todas las estaciones.}
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
 #' }
 #' @source Repositorio de GitHub.
-"descargar_datos"
+"NH0098"
+
+#' Datos de la estación meteorológica NH0046
+#'
+#' Este conjunto de datos incluye mediciones de la estación meteorológica "NH0046".
+#'
+#' @format Un dataframe con las siguientes columnas:
+#' \describe{
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
+#' }
+#' @source Repositorio de GitHub.
+"NH0046"
+
+#' Datos de la estación meteorológica NH0437
+#'
+#' Este conjunto de datos incluye mediciones de la estación meteorológica "NH0437".
+#'
+#' @format Un dataframe con las siguientes columnas:
+#' \describe{
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
+#' }
+#' @source Repositorio de GitHub.
+"NH0437"
+
+#' Datos de la estación meteorológica NH0472
+#'
+#' Este conjunto de datos incluye mediciones de la estación meteorológica "NH0472".
+#'
+#' @format Un dataframe con las siguientes columnas:
+#' \describe{
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
+#' }
+#' @source Repositorio de GitHub.
+"NH0472"
+
+#' Datos de la estación meteorológica NH0910
+#'
+#' Este conjunto de datos incluye mediciones de la estación meteorológica "NH0910".
+#'
+#' @format Un dataframe con las siguientes columnas:
+#' \describe{
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
+#' }
+#' @source Repositorio de GitHub.
+"NH0910"
+
+#' Datos de todas las estaciones meteorológicas
+#'
+#' Este conjunto de datos incluye mediciones de todas las estaciones disponibles.
+#'
+#' @format Un dataframe con las siguientes columnas:
+#' \describe{
+#'   \item{id}{ID de la estación realizada.}
+#'   \item{fecha}{Fecha de la medición.}
+#'   \item{temperatura_abrigo_150cm}{Temperatura medida a 150 cm de altura.}
+#'   \item{temperatura_abrigo_150cm_maxima}{Temperatura máxima a 150 cm.}
+#'   \item{temperatura_abrigo_150cm_minima}{Temperatura mínima a 150 cm.}
+#'   \item{temperatura_intemperie_5cm_minima}{Temperatura mínima en intemperie a 5 cm.}
+#'   \item{temperatura_intemperie_50cm_minima}{Temperatura mínima en intemperie a 50 cm.}
+#'   \item{temperatura_suelo_5cm_media}{Temperatura media del suelo a 5 cm.}
+#'   \item{temperatura_suelo_10cm_media}{Temperatura media del suelo a 10 cm.}
+#'   \item{temperatura_inte_5cm}{Temperatura interna a 5 cm.}
+#'   \item{temperatura_intemperie_150cm_minima}{Temperatura mínima en intemperie a 150 cm.}
+#'   \item{humedad_suelo}{Humedad del suelo.}
+#'   \item{precipitacion_pluviometrica}{Precipitación medida.}
+#'   \item{granizo}{1 si hubo granizo y 0 si no hubo.}
+#'   \item{nieve}{1 si hubo nieve y 0 si no hubo.}
+#'   \item{heliofania_efectiva}{Heliofanía efectiva.}
+#'   \item{heliofania_relativa}{Heliofanía relativa.}
+#'   \item{tesion_vapor_media}{Tensión de vapor media.}
+#'   \item{humedad_media}{Humedad media.}
+#'   \item{humedad_media_8_14_20}{Humedad media de 8 a 14 y 20.}
+#'   \item{rocio_medio}{Rocío medio.}
+#'   \item{duracion_follaje_mojado}{Duración del follaje mojado.}
+#'   \item{velocidad_viento_200cm_media}{Velocidad media del viento a 200 cm.}
+#'   \item{direccion_viento_200cm}{Dirección del viento a 200 cm.}
+#'   \item{velocidad_viento_1000cm_media}{Velocidad media del viento a 1000 cm.}
+#'   \item{direccion_viento_1000cm}{Dirección del viento a 1000 cm.}
+#'   \item{velocidad_viento_maxima}{Velocidad máxima del viento.}
+#'   \item{presion_media}{Presión media.}
+#'   \item{radiacion_global}{Radiación global.}
+#'   \item{radiacion_neta}{Radiación neta.}
+#'   \item{evaporacion_tanque}{Evaporación en tanque.}
+#'   \item{evapotranspiracion_potencial}{Evapotranspiración potencial.}
+#'   \item{profundidad_napa}{Profundidad de napa.}
+#'   \item{horas_frio}{Horas de frío.}
+#'   \item{unidad_frio}{Unidad de frío.}
+#' }
+#' @source Repositorio de GitHub.
+"NH_unidos"
